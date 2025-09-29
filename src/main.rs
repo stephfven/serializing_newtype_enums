07b3ace93b461b42a977a02158a71e72f3c329bf
Stefan Mykytyn <swmykytyn@scires.com>
2025-09-29 12:21:34 -0400

    Renamed example objects; Introduced new struct field that is having issues with deserialization. Got an awkward raw string-parsing solution working, but would like to use an enum for a more elegant solution

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::path::PathBuf;
 
 use crate::newtype_variant_enum::{
-    DeviceAttributes,
-    types::ControlType,
+    Product,
+    types::{Currency, Sale},
     xml::{from_xml_file, to_xml_file},
 };
 
@@ -11,12 +11,37 @@ mod newtype_variant_enum;
 
 fn main() {
     let file_path = PathBuf::from("test.xml");
-    let obj = DeviceAttributes {
-        name: "OtherDeviceTest".to_string(),
-        ctrl_type: ControlType::Voltage(2.0),
+    let obj = Product {
+        name: "Scrub Daddy".to_string(),
+        price: Currency::Dollars(6.0),
+        sale: Some(Sale(25.5)),
     };
 
-    println!("\n\n[Original] {}, {:?}", obj.name, obj.ctrl_type);
+    println!("\n\nWith a rating:");
+    println!("\n[Original] {}, {:?}, {:?}", obj.name, obj.price, obj.sale);
+
+    // Export
+    to_xml_file(&file_path, &obj).expect("should have written object to file");
+    println!("\n\n[EXPORT] Successfully wrote object to file.");
+
+    // Import
+    let res = from_xml_file(&file_path).expect("should have read object into memory");
+    println!("\n\n[IMPORT] Successfully imported object from file.");
+
+    assert_eq!(res, obj, "imported object does not match original");
+    println!("\n[IMPORT] Imported object matches original.");
+
+    println!("\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
+
+    let file_path = PathBuf::from("test.xml");
+    let obj = Product {
+        name: "F-22 Raptor".to_string(),
+        price: Currency::Dollars(350000000.0),
+        sale: None,
+    };
+
+    println!("Without a rating:");
+    println!("\n[Original] {}, {:?}, {:?}", obj.name, obj.price, obj.sale);
 
     // Export
     to_xml_file(&file_path, &obj).expect("should have written object to file");
--- src/newtype_variant_enum.rs
+++ src/newtype_variant_enum.rs
@@ -1,24 +1,33 @@
 use serde::{Deserialize, Serialize};
 
-use crate::newtype_variant_enum::types::serialize_ctrl_type::deserialize_flattened;
+use crate::newtype_variant_enum::types::{
+    parse_sale_or_empty_string, serialize_currency::deserialize_flattened,
+};
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "PascalCase")]
-pub struct DeviceAttributes {
+pub struct Product {
     pub name: String,
     #[serde(flatten, deserialize_with = "deserialize_flattened")]
-    pub ctrl_type: types::ControlType,
+    pub price: types::Currency,
+    #[serde(deserialize_with = "parse_sale_or_empty_string")]
+    pub sale: Option<types::Sale>,
 }
 
 pub mod types {
+    use serde::de::{Deserializer, Error};
+
     use super::*;
 
     #[derive(Debug, PartialEq, Serialize, Deserialize)]
-    pub enum ControlType {
-        Voltage(f32),
-        Power(f32),
+    pub struct Sale(pub f32);
+
+    #[derive(Debug, PartialEq, Serialize, Deserialize)]
+    pub enum Currency {
+        Dollars(f32),
+        Euros(f32),
     }
-    pub mod serialize_ctrl_type {
+    pub mod serialize_currency {
         use super::*;
         use serde::de::{self, MapAccess, Visitor};
         use std::fmt;
@@ -36,10 +45,10 @@ pub mod types {
         struct ControlVisitor;
 
         impl<'de> Visitor<'de> for ControlVisitor {
-            type Value = ControlType;
+            type Value = Currency;
 
             fn expecting(&self, f: &mut fmt::Formatter) -> fmt::Result {
-                f.write_str("Power or Voltage element")
+                f.write_str("Euros or Dollars element")
             }
 
             fn visit_map<M>(self, mut map: M) -> Result<Self::Value, M::Error>
@@ -54,20 +63,34 @@ pub mod types {
                     };
                     let f = s.parse::<f32>().map_err(de::Error::custom)?;
                     return match key.as_str() {
-                        "Power" => Ok(ControlType::Power(f)),
-                        "Voltage" => Ok(ControlType::Voltage(f)),
+                        "Euros" => Ok(Currency::Euros(f)),
+                        "Dollars" => Ok(Currency::Dollars(f)),
                         _ => Err(de::Error::custom(format!("unexpected key {}", key))),
                     };
                 }
-                Err(de::Error::custom("expected <Power> or <Voltage> element"))
+                Err(de::Error::custom("expected <Euros> or <Dollars> element"))
             }
         }
 
-        pub fn deserialize_flattened<'de, D>(deserializer: D) -> Result<ControlType, D::Error>
+        pub fn deserialize_flattened<'de, D>(deserializer: D) -> Result<Currency, D::Error>
         where
             D: de::Deserializer<'de>,
         {
             deserializer.deserialize_map(ControlVisitor)
+        }
+    }
+
+    pub fn parse_sale_or_empty_string<'de, D>(deserializer: D) -> Result<Option<Sale>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        // WORKING but not elegant - try this approach using an enum: https://users.rust-lang.org/t/serde-deserialize-empty-string-as-option-none/116201/2
+        match String::deserialize(deserializer) {
+            Ok(str) if str.is_empty() => Ok(None),
+            Ok(str) => Ok(Some(Sale(str.parse::<f32>().map_err(|err| {
+                D::Error::custom(format!("unexpected nonempty string: `{err}`"))
+            })?))),
+            Err(err) => Err(err),
         }
     }
 }
@@ -75,24 +98,21 @@ pub mod types {
 pub mod xml {
     use std::{fs::File, io::BufReader, path::PathBuf};
 
-    use super::DeviceAttributes;
+    use super::Product;
 
-    pub fn from_xml_file(file_path: impl Into<PathBuf>) -> Result<DeviceAttributes, String> {
+    pub fn from_xml_file(file_path: impl Into<PathBuf>) -> Result<Product, String> {
         let file_path = file_path.into();
         let source: File = File::open(&file_path)
             .map_err(|e| format!("failed to open file: {:?}", e.to_string()))?;
         let reader: BufReader<File> = BufReader::new(source);
 
-        let output: DeviceAttributes = quick_xml::de::from_reader(reader)
+        let output: Product = quick_xml::de::from_reader(reader)
             .map_err(|e| format!("failed to deserialize: {:?}", e.to_string()))?;
 
         Ok(output)
     }
 
-    pub fn to_xml_file(
-        file_path: impl Into<PathBuf>,
-        obj: &DeviceAttributes,
-    ) -> Result<File, String> {
+    pub fn to_xml_file(file_path: impl Into<PathBuf>, obj: &Product) -> Result<File, String> {
         let file: File = File::create(file_path.into())
             .map_err(|e| format!("failed to create file: {:?}", e.to_string()))?;
         let mut writer: quick_xml::Writer<&File> = quick_xml::Writer::new(&file);
@@ -107,51 +127,72 @@ pub mod xml {
 
 #[cfg(test)]
 pub mod tests {
+    use crate::newtype_variant_enum::types::Sale;
+    use pretty_assertions::assert_eq;
     use std::path::PathBuf;
 
     use super::{
-        DeviceAttributes,
-        types::ControlType,
+        Product,
+        types::Currency,
         xml::{from_xml_file, to_xml_file},
     };
 
     #[test]
-    fn export() {
+    fn export_only() {
         let file_path = PathBuf::from("export.xml");
-        let out = DeviceAttributes {
-            name: "MyDevice".to_string(),
-            ctrl_type: ControlType::Power(3.5),
+        let out = Product {
+            name: "Fidget Spinner".to_string(),
+            price: Currency::Euros(3.5),
+            sale: None,
         };
 
         to_xml_file(&file_path, &out).expect("should have written object to file");
     }
 
     #[test]
-    fn import() {
+    fn import_only() {
         let file_path = PathBuf::from("import.xml");
-        let out = DeviceAttributes {
-            name: "MyDevice".to_string(),
-            ctrl_type: ControlType::Power(3.5),
+        let exp = Product {
+            name: "Fidget Spinner".to_string(),
+            price: Currency::Euros(3.5),
+            sale: None,
         };
 
         let res = from_xml_file(&file_path).expect("should have read object into memory");
-        assert_eq!(res, out, "imported object does not match original");
+        assert_eq!(res, exp, "imported object does not match original");
     }
 
     #[test]
-    #[ignore]
-    fn export_and_import() {
+    fn both_export_and_import_without_rating() {
         let file_path = PathBuf::from("test.xml");
-        let out = DeviceAttributes {
-            name: "OtherDeviceTest".to_string(),
-            ctrl_type: ControlType::Voltage(2.0),
+        let obj = Product {
+            name: "F-22 Raptor".to_string(),
+            price: Currency::Dollars(350000000.0),
+            sale: None,
         };
 
         // Export
-        to_xml_file(&file_path, &out).expect("should have written object to file");
+        to_xml_file(&file_path, &obj).expect("should have written object to file");
 
         // Import
         let res = from_xml_file(&file_path).expect("should have read object into memory");
-        assert_eq!(res, out, "imported object does not match original");
+        assert_eq!(res, obj, "imported object does not match original");
+    }
+
+    #[test]
+    fn both_export_and_import_with_rating() {
+        let file_path = PathBuf::from("test1.xml");
+        let obj = Product {
+            name: "Scrub Daddy".to_string(),
+            price: Currency::Dollars(6.0),
+            sale: Some(Sale(25.5)),
+        };
+
+        // Export
+        to_xml_file(&file_path, &obj).expect("should have written object to file");
+
+        // Import
+        let res = from_xml_file(&file_path).expect("should have read object into memory");
+        assert_eq!(res, obj, "imported object does not match original");
     }
 }
